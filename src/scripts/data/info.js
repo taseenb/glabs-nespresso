5f74378f7038bbaccf6e7800c2f85c0cbf061576
taseenb <esteban.almiron@gmail.com>
2016-08-19 14:01:29 +0100

    update

--- src/scripts/data/info.js
+++ src/scripts/data/info.js
@@ -1,13 +1,13 @@
 define(function (require) {
 
   return {
-    pageUrl: 'https://labs.theguardian.com/2016/nespresso/history-of-coffee/',
+    pageUrl: 'http://www.theguardian.com/discover-nespresso/ng-interactive/2016/aug/19/14-landmarks-in-the-surprising-history-of-coffee/',
 
     googleAnalyticsId: 'UA-72576373-2',
     facebookAppId: '741666719251986',
     facebookTitle: 'A Brief History of Coffee',
     facebookText: 'The Sufis used it for prayer. The Muslims drank it under pain of death. The Pope baptised it, and Bach wrote a cantata to it. Discover 14 surprising coffee landmarks, courtesy of Nespresso.',
-    twitterText: 'Life might be measured out in coffee spoons, but the history of coffee can be measured in 14 key events. Nespresso explains all...',
+    twitterText: 'Life might be measured out in coffee spoons, but the history of coffee can be measured in 14 key events.',
 
   };
 
